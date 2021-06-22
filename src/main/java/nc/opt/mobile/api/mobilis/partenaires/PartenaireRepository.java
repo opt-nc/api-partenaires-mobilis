2d18ab9f89ed5492941a24396c8bcec9c19ac8aa
Daniel Santos <daniel.santos.ext@opt.nc>
2021-06-23 08:46:46 +1100

    feat(partenaires) Exposition de la listes des partenaires en REST : en lecture seule
    SIARUN-1682

--- src/main/java/nc/opt/mobile/api/mobilis/partenaires/PartenaireRepository.java
+++ src/main/java/nc/opt/mobile/api/mobilis/partenaires/PartenaireRepository.java
@@ -2,10 +2,18 @@ package nc.opt.mobile.api.mobilis.partenaires;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Repository Spring Boot avec accès REST
  */
 @RepositoryRestResource(collectionResourceRel = "partenaire", path = "partenaires")
 public interface PartenaireRepository extends PagingAndSortingRepository<Partenaire, Long> {
+
+    /**
+     * Pas de possibilité de création/modif en REST
+     */
+    @Override
+    @RestResource(exported = false)
+    <S extends Partenaire> S save(S entity);
 }
