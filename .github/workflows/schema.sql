1a5beacfd469588b4524c8dbc2c21865e1d26f55
Daniel Santos <daniel.santos@redstone.nc>
2021-09-13 13:44:12 +1100

    chore(ci) Validation structure CSV sur Postgres via les GitHub actions
    Tnetative en utilisant la directive "cotainer" qui permet d'éxécuter les steps sous jacents dedans
    
    #10

---
+++ .github/workflows/schema.sql
@@ -0,0 +1,14 @@
+-- l'ordre est le même que sur le fichier CSV pour éviter de lister les colonnes dans la commande COPY
+create table partenaire (
+  id bigint not null,
+  nom varchar(255) not null,
+  ridet varchar(255),
+  adresse varchar(255) not null,
+  telephone varchar(255),
+  url_gmaps varchar(255) check(url_gmaps is null or url_gmaps like 'https://%'),
+  url_fb varchar(255) check(url_fb is null or url_fb like 'https://%'),
+  quartier varchar(255),
+  ville varchar(255) not null,
+  position varchar(255) not null,
+  primary key (id)
+);
